8eb19ffa5a6b53664cee65bb5fe8433f34aa04e1
fedeRomer <60818032+fedeRomer@users.noreply.github.com>
2020-08-26 20:32:09 -0300

    update Exceptions

--- src/com/AppBackend/exceptions/NoSuchIdException.java
+++ src/com/AppBackend/exceptions/NoSuchIdException.java
@@ -1,14 +1,14 @@
-package com.AppBackend.Exceptions;
+package com.AppBackend.exceptions;
 
 import java.io.IOException;
 
-public class NoSuchUserException extends IOException {
+public class NoSuchIdException extends IOException {
     /**
 	 * 
 	 */
 	private static final long serialVersionUID = -3163983489146457190L;
 
-	public NoSuchUserException(String message) {
+	public NoSuchIdException(String message) {
         super(message);
     }
 }
