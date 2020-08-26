9ad89210c829f822e661cbde678d8703c38e7c38
fedeRomer <60818032+fedeRomer@users.noreply.github.com>
2020-08-26 20:48:18 -0300

    Update AerolineaDAOimpl.java

--- src/com/AppBackend/dao/daoImplementations/AerolineaDAOimpl.java
+++ src/com/AppBackend/dao/daoImplementations/AerolineaDAOimpl.java
@@ -1,7 +1,41 @@
 package com.AppBackend.dao.daoImplementations;
+import java.util.List;
+
 import com.AppBackend.dao.daoInterfaces.AerolineaDAO;
+import com.AppBackend.domain.Aerolinea;
+import com.AppBackend.exceptions.NoSuchIdException;
 
 public class AerolineaDAOimpl implements AerolineaDAO{
+
+	@Override
+	public void addAerolinea(Aerolinea a) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Aerolinea updateAerolinea(Aerolinea a, int idAerolinea) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteAerolinea(int idAerolinea) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Aerolinea> findAllAerolinea() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Aerolinea getAerolinea(int aerolinea) throws NoSuchIdException {
+		// TODO Auto-generated method stub
+		return null;
+	}
 	
 
 }
