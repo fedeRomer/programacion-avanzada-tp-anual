ab579382d4c2067d2a5b52980c51257192e37de6
fedeRomer <60818032+fedeRomer@users.noreply.github.com>
2020-08-26 20:46:49 -0300

    Update AerolineaDAO.java

--- src/com/AppBackend/dao/daoInterfaces/AerolineaDAO.java
+++ src/com/AppBackend/dao/daoInterfaces/AerolineaDAO.java
@@ -1,7 +1,16 @@
 package com.AppBackend.dao.daoInterfaces;
 
+import java.util.List;
+
+import com.AppBackend.domain.Aerolinea;
+import com.AppBackend.exceptions.NoSuchIdException;
+
 public interface AerolineaDAO {
 	
-	
+	public void addAerolinea(Aerolinea a);
+	public Aerolinea updateAerolinea(Aerolinea a, int idAerolinea);
+	public void deleteAerolinea(int idAerolinea);
+	public List<Aerolinea> findAllAerolinea();
+	public Aerolinea getAerolinea(int aerolinea) throws NoSuchIdException;
 
 }
