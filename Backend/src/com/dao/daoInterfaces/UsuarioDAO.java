50a711ef6ca400604e1634599e17515615124290
fedeRomer <60818032+fedeRomer@users.noreply.github.com>
2021-02-20 12:33:59 -0300

    Update UsuarioDAO.java
    support for boolean response adduser deleteuser

--- Backend/src/com/dao/daoInterfaces/UsuarioDAO.java
+++ Backend/src/com/dao/daoInterfaces/UsuarioDAO.java
@@ -7,9 +7,10 @@ import com.exceptions.NoSuchIdException;
 
 public interface UsuarioDAO {
 
-    public void addUsuario(Usuario Usuario);
+    public Boolean addUsuario(Usuario Usuario);
     public Usuario updateUsuario(Usuario Usuario);
-    public void deleteUsuario(int id);
+    public Boolean deleteUsuario(int id);
     public List<Usuario> findAllUsuario();
     public Usuario getUsuario(int id) throws NoSuchIdException;
+    public Usuario getUsuario(String user, String password);
 }
