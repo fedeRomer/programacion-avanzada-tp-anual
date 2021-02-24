0c519aa2119f2ef9e6ba66b275741534f7e86ea0
fedeRomer <60818032+fedeRomer@users.noreply.github.com>
2021-02-24 19:05:47 -0300

    Update UsuarioBLL.java

--- Backend/src/com/bll/UsuarioBLL.java
+++ Backend/src/com/bll/UsuarioBLL.java
@@ -1,12 +1,32 @@
 package com.bll;
 
+import java.io.IOException;
+import java.sql.SQLException;
+
+import com.dao.daoFactories.UsuarioDAOFactory;
+import com.dao.daoInterfaces.UsuarioDAO;
 import com.domain.Usuario;
 
 public class UsuarioBLL {
-	
-	public boolean validatePassword(Usuario u) {
-		//validate con db
-		return true;
+
+	public boolean validatePassword(Usuario u) throws SQLException, IOException {
+		// validate con db
+		
+		UsuarioDAO usuarioDAO = UsuarioDAOFactory.getUsuarioDAOimpl();
+		
+		u = usuarioDAO.getUsuario(u.getUsername(), u.getPassword());
+		
+		if (u != null) {
+			
+			return true;
+			
+		} else {
+			
+			return false;
+			
+		}
+
+		// return true; -> mockear para validar sesion
 	}
 
 }
