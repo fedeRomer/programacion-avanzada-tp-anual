0652677c6523066c6c8c3ac19b85dad9944fef04
fedeRomer <60818032+fedeRomer@users.noreply.github.com>
2021-03-22 18:29:57 -0300

    Update ProvinciasDAOimpl.java

--- Backend/src/com/dao/daoImplementations/ProvinciasDAOimpl.java
+++ Backend/src/com/dao/daoImplementations/ProvinciasDAOimpl.java
@@ -3,8 +3,10 @@ package com.dao.daoImplementations;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.dao.daoInterfaces.ProvinciaDAO;
@@ -41,25 +43,84 @@ public class ProvinciasDAOimpl implements ProvinciaDAO{
 	}
 
 	@Override
-	public Boolean updateProvincia(Provincia Provincia) {
-		return null;
+	public Boolean updateProvincia(Provincia provincia) throws SQLException, IOException {
+		MySQL mySQL = new MySQL();
+		this.connection = mySQL.getConnection();
+		query = "UPDATE PROVINCIA SET nombre = ?";
+		preparedStatement = connection.prepareStatement(query);
+		preparedStatement.setString(1, provincia.getNombre());
+
+		int resultSet = preparedStatement.executeUpdate();
+		if (resultSet == 1) {
+			preparedStatement.close();
+			connection.close();
+			return true;
+		} else {
+			preparedStatement.close();
+			connection.close();
+			return false;
+		}
 	}
 
 	@Override
-	public Boolean deleteProvincia(int id) {
-		return null;
+	public Boolean deleteProvincia(int id) throws SQLException, IOException {
+		MySQL mySQL = new MySQL();
+		this.connection = mySQL.getConnection();
+		query = "DELETE FROM PROVINCIA WHERE id = ?";
+		preparedStatement = connection.prepareStatement(query);
+		preparedStatement.setInt(1, id);
+		
+		int resultSet = preparedStatement.executeUpdate();
+		if (resultSet == 1) {
+			preparedStatement.close();
+			connection.close();
+			return true;
+		} else {
+			preparedStatement.close();
+			connection.close();
+			return false;
+		}
 	}
 
 	@Override
-	public List<Provincia> findAllProvincia() {
-		// TODO Auto-generated method stub
-		return null;
+	public List<Provincia> findAllProvincia() throws SQLException, IOException {
+		MySQL mySQL = new MySQL();
+		this.connection = mySQL.getConnection();
+		List<Provincia> provinciaList = new ArrayList<Provincia>();
+		query = "select * from PROVINCIA";
+		preparedStatement = connection.prepareStatement(query);
+		ResultSet resultSet = preparedStatement.executeQuery();
+		while (resultSet.next()) {
+			Provincia p = new Provincia();
+			p.setId(resultSet.getInt(1));
+			p.setNombre(resultSet.getString(2));
+			provinciaList.add(p);
+		}
+		resultSet.close();
+		preparedStatement.close();
+		connection.close();
+		
+		return provinciaList;
 	}
 
 	@Override
-	public Provincia getProvincia(int id) throws NoSuchIdException {
-		// TODO Auto-generated method stub
-		return null;
+	public Provincia getProvincia(int id) throws NoSuchIdException, SQLException, IOException {
+		MySQL mySQL = new MySQL();
+		this.connection = mySQL.getConnection();
+		query = "select * from PROVINCIA where id = ?";
+		preparedStatement = connection.prepareStatement(query);
+		preparedStatement.setInt(1, id);
+
+		ResultSet set = preparedStatement.executeQuery();
+		Provincia p = new Provincia();
+		while (set.next()) {
+			p.setId(set.getInt(1));
+			p.setNombre(set.getString(2));
+		}
+		set.close();
+		preparedStatement.close();
+		connection.close();
+		return p;
 	}
 
 }
